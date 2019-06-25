3ce9ebe50e8f87aaf33c1a3e0921f1b04271d3bc
DuldR <gnjulaka@gmail.com>
2019-06-24 23:03:45 -0500

    Submitted all exercises and downloaded solutions.

--- Scope and References/Scope Exercises/junk.rb
+++ Scope and References/Scope Exercises/junk.rb
@@ -1,13 +1,17 @@
-weiner = "weiner"
-butts = weiner
+def scalar_multiple!(array, multiplier)
+    array.map! { |el| el * multiplier }
+end
 
-p weiner.object_id
-p butts.object_id
 
-weiner[0] = "d"
+array_1 = [7, 0, 4]
+p array_1.object_id                             # => 70326264380840
+result_1 = scalar_multiple!(array_1, 3)
+p result_1                                      # => [21, 0, 12]
+p result_1.object_id                            # => 70326264380840
 
-p weiner
-p butts
 
-p weiner.object_id
-p butts.object_id
+array_2 = [90, 30, 4, 12]
+p array_2.object_id                             # => 70326264378100
+result_2 = scalar_multiple!(array_2, 0.5)
+p result_2                                      # => [45.0, 15.0, 2.0, 6.0]
+p result_2.object_id             
