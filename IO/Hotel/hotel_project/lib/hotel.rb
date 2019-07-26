9159b92d0a005c437baa2505f4b5182dc3042d6f
DuldR <gnjulaka@gmail.com>
2019-07-26 06:06:35 -0500

    Hotel California Complete
    VERY Involved project. Really enjoyed completing this one. Difficult time understand setting hash key to new room object.

--- IO/Hotel/hotel_project/lib/hotel.rb
+++ IO/Hotel/hotel_project/lib/hotel.rb
@@ -2,4 +2,66 @@ require_relative "room"
 
 class Hotel
   
+
+    #----Initialization----
+    def initialize(name, rooms)
+        @name = name
+
+        #Sets each room NAME to a new room object.
+        @rooms = rooms.each { |k, v| rooms[k] = Room.new(v) }
+
+    end
+
+    #----Getter----
+    def rooms
+        @rooms
+    end
+
+    def name
+        #Split the string, capitalize and rejoin.
+        @name = @name.split(" ").map { |ele| ele.capitalize }.join(" ")
+    end
+
+     #----Instance Method----
+
+    def room_exists?(roomName)
+        if @rooms.include?(roomName) == true
+            return true
+        else
+            return false
+
+        end
+    end
+
+    def check_in(person, roomName)
+
+        if self.room_exists?(roomName) == true
+            if @rooms[roomName].add_occupant(person) == true
+                print "check in successful"
+            else
+                print "sorry, room is full"
+            end
+        else
+            print "sorry, room does not exist"
+        end
+    end
+
+    def has_vacancy?
+
+        if @rooms.any? { |k,v| v.available_space > 0 } == true
+            return true
+        else
+            return false
+        end
+ 
+    end
+
+    def list_rooms
+        @rooms.each do |k, v|
+            print k + v.available_space.to_s
+            print "\n"
+        end
+    end
+
+
 end
--- IO/Hotel/hotel_project/lib/room.rb
+++ IO/Hotel/hotel_project/lib/room.rb
@@ -1,3 +1,45 @@
 class Room
 
+    #----Initialization----
+
+    def initialize(capacity)
+        @capacity = capacity
+        @occupants = []
+    end
+
+     #----Getter Methods----
+
+     def capacity
+        @capacity
+     end
+
+     def occupants
+        @occupants
+     end
+
+      #----Instance Method----
+
+    def full?
+        if @occupants.length < @capacity
+            return false
+        else
+            return true
+        end
+    end
+
+    def available_space
+        return (@capacity - @occupants.length)
+    end
+
+    def add_occupant(name)
+        if self.full? == true
+            return false
+        else
+            @occupants << name
+            return true
+        end
+
+    end
+
+    
 end
